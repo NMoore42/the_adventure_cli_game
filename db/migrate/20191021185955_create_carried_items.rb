7570e0b6fe109db8a5aa6e77ac2d4a19999d87ef
NMoore42 <nicholas.moore5@icloud.com>
2020-01-06 17:11:51 -0600

    test

--- db/migrate/20191021185955_create_carried_items.rb
+++ db/migrate/20191021185955_create_carried_items.rb
@@ -3,6 +3,7 @@ class CreateCarriedItems < ActiveRecord::Migration[5.2]
     create_table :carried_items do |t|
       t.integer :player_id
       t.integer :item_id
+      #this is new code, it lives on the the test branch - not the master
     end
   end
 end
